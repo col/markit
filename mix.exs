2ec11e1aa723f1ac5b23f3e67a5dbda4bc2bc8c9
Colin Harris <col.w.harris@gmail.com>
2016-03-04 00:45:32 +0800

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Markit.Mixfile do
 
   def project do
     [app: :markit,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
